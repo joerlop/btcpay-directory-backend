cad2313d7957cf56fc105d45e6f09e19eed765af
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2020-03-03 17:14:30 -0500

    Companies model and router structure developed

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const companiesRouter = require('../companies/companies-router.js');
+
 const server = express();
+
+server.use('/api/companies', companiesRouter);
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
