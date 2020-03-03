45153c553b22e74f1779ecb3f44ae4b0025e1fd3
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2020-03-03 10:52:49 -0500

    Knexfile done

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,32 @@
-// Update with your config settings.
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './database/auth.db3',
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "pg",
+    connection: dbConnection,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 };
